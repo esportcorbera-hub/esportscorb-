0290d2b5966fb2540a0945b237feb6c6741faf19
esportcorbera-hub <esportcorbera@gmail.com>
2026-09-24 17:42:34 +0200

    Provision TECNIC staff account securely

--- seed_entities.py
+++ seed_entities.py
@@ -6,7 +6,7 @@ from pathlib import Path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'esportscorb.settings')
 django.setup()
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 from django.db import transaction
 from django.utils.text import slugify
 
@@ -44,5 +44,31 @@ def seed_entities():
     print(f'Entity import complete: {created} created, {skipped} already present.')
 
 
+def seed_technician():
+    password = os.environ.get('TECHNIC_INITIAL_PASSWORD')
+    if not password:
+        print('Technician account skipped: TECHNIC_INITIAL_PASSWORD is not configured.')
+        return
+
+    with transaction.atomic():
+        user, created = User.objects.get_or_create(
+            username='TECNIC',
+            defaults={'is_staff': True},
+        )
+        if created:
+            user.set_password(password)
+            user.save(update_fields=['password'])
+        elif not user.is_staff:
+            user.is_staff = True
+            user.save(update_fields=['is_staff'])
+
+        group, _ = Group.objects.get_or_create(name='Tècnic')
+        user.groups.add(group)
+
+    state = 'created' if created else 'already present'
+    print(f'Technician account {state}; staff access ensured.')
+
+
 if __name__ == '__main__':
     seed_entities()
+    seed_technician()
